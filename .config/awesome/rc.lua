f5b48fd0a818e6f6a59ebb956fb795c90077ac88
Vivek Jagannath <vivekj1203@gmail.com>
2021-07-27 16:32:31 +0530

    Added home directory files

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -402,7 +402,7 @@ client.connect_signal("manage", function (c)
         awful.placement.no_offscreen(c)
     end
 end)
-
+}
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
